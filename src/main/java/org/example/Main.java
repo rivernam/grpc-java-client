d8ea11f90136ea737c2b774b282c303061bf1660
rivernam <nam829.kang@gmail.com>
2023-06-07 23:06:45 +0900

    -replace System.out -> logger.info -add sleep option to control loop speed -clean some code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -32,9 +32,10 @@ public class Main implements Runnable {
     String proxyServer;
     @Option(names = {"-pp", "--proxy-port"}, description = "proxy server port")
     int proxyPort;
-
     @Option(names = {"-m", "--mode"}, description = "grpc client request mode. You have to choose between blocking, async and future", required = true)
     String grpcMode;
+    @Option(names={"-s", "--sleep"}, defaultValue = "1")
+    int sleep;
 
     public static void main(String[] args) {
         int exitCode = new CommandLine(new Main()).execute(args);
@@ -48,67 +49,50 @@ public class Main implements Runnable {
         for (int i = 0; i < loop; i++) {
             ManagedChannel channel = buildGrpcChannel();
 
-            if(grpcMode.equals("blocking")){
+            logger.info("{} say hello before", i);
+            if (grpcMode.equals("block")) {
                 grpcBlocking(i, channel);
-            }
-            else if(grpcMode.equals("async"))
-            {
-                try {
-                    grpcAsync(i, channel);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-            else{
+            } else if (grpcMode.equals("async")) {
+                grpcAsync(i, channel);
+            } else if (grpcMode.equals("future")){
                 grpcFuture(i, channel);
             }
-
+            else{
+                throw new IllegalArgumentException("argument \"" + grpcMode + "\" is invalid");
+            }
             logger.info("{} say hello after", i);
-        }
-    }
 
-    private void grpcFuture(int i, ManagedChannel channel) {
-        HelloServiceGrpc.HelloServiceFutureStub futureStub = HelloServiceGrpc.newFutureStub(channel);
-
-        HelloRequest request = HelloRequest.newBuilder().setIndex(i).setRequestTime(LocalDateTime.now().toString()).build();
-        System.out.println(i + " say hello before");
-        try {
-            HelloResponse response = futureStub.sayHello(request).get(3, TimeUnit.SECONDS);
-            System.out.println(response.getMessage());
-            channel.shutdown();
-            System.out.println(i + " channel shutdown");
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        } catch (ExecutionException e) {
-            throw new RuntimeException(e);
-        } catch (TimeoutException e) {
-            throw new RuntimeException(e);
+            try {
+                Thread.sleep(sleep);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 
     private void grpcBlocking(int i, ManagedChannel channel) {
-        HelloServiceGrpc.HelloServiceBlockingStub blockingStub = HelloServiceGrpc.newBlockingStub(channel);
+        HelloServiceGrpc.HelloServiceBlockingStub blockingStub = HelloServiceGrpc.newBlockingStub(channel)
+                .withDeadlineAfter(5, TimeUnit.SECONDS);
 
         HelloRequest request = HelloRequest.newBuilder().setIndex(i).setRequestTime(LocalDateTime.now().toString()).build();
-        System.out.println(i + " say hello before");
 
         HelloResponse response = blockingStub.sayHello(request);
-        System.out.println(response.getMessage());
+        logger.info(response.getMessage());
 
         channel.shutdown();
-        System.out.println(i + " channel shutdown");
+        logger.info("{} channel shutdown", i);
     }
 
-    private void grpcAsync(int i, ManagedChannel channel) throws InterruptedException {
-        HelloServiceGrpc.HelloServiceStub asyncStub = HelloServiceGrpc.newStub(channel);
+    private void grpcAsync(int i, ManagedChannel channel) {
+        HelloServiceGrpc.HelloServiceStub asyncStub = HelloServiceGrpc.newStub(channel)
+                .withDeadlineAfter(5, TimeUnit.SECONDS);
 
         HelloRequest request = HelloRequest.newBuilder().setIndex(i).setRequestTime(LocalDateTime.now().toString()).build();
-        logger.info("{} say hello before", i);
 
         asyncStub.sayHello(request, new StreamObserver<HelloResponse>() {
             @Override
             public void onNext(HelloResponse helloResponse) {
-                logger.info("on next, {}",helloResponse.getMessage());
+                logger.info("on next, {}", helloResponse.getMessage());
             }
 
             @Override
@@ -123,13 +107,28 @@ public class Main implements Runnable {
                 logger.info("{} channel shutdown", i);
             }
         });
-        Thread.sleep(10);
+    }
+
+    private void grpcFuture(int i, ManagedChannel channel) {
+        HelloServiceGrpc.HelloServiceFutureStub futureStub = HelloServiceGrpc.newFutureStub(channel)
+                .withDeadlineAfter(5, TimeUnit.SECONDS);
+
+        HelloRequest request = HelloRequest.newBuilder().setIndex(i).setRequestTime(LocalDateTime.now().toString()).build();
+        HelloResponse response;
+        try {
+            response = futureStub.sayHello(request).get(3, TimeUnit.SECONDS);
+            logger.info(response.getMessage());
+        } catch (InterruptedException | ExecutionException | TimeoutException e) {
+            logger.error("future get exception", e);
+        }
+        channel.shutdown();
+        logger.info("{} channel shutdown", i);
     }
 
     private ManagedChannel buildGrpcChannel() {
         ManagedChannelBuilder<?> channelBuilder = ManagedChannelBuilder.forAddress(host, port)
                 .usePlaintext();
-        if(Objects.nonNull(proxyServer) &&  !proxyServer.isEmpty() && proxyPort != 0){
+        if (Objects.nonNull(proxyServer) && !proxyServer.isEmpty() && proxyPort != 0) {
             configureProxyServer(channelBuilder);
         }
         return channelBuilder.build();
